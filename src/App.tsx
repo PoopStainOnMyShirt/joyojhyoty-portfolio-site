55b143cfe33bc1b6ce4b943d41a9efb252a7d6c4
PoopStainOnMyShirt <joyojhyoty@gmail.com>
2025-11-30 20:00:58 +0530

    fix: path

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 
 // 1. Define the BASE_PATH conditionally
 // Use the repository name ONLY in production (on GitHub Pages)
-const BASE_PATH = import.meta.env.PROD ? "/joyojhyoty-portfolio-site/" : "/";
+const BASE_PATH = import.meta.env.DEV ? "/" : "/joyojhyoty-portfolio-site/";
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
