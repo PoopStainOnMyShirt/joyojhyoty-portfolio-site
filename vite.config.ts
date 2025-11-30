b98ad84ee9e271e9c636b3f0ba33c486e7123bcf
PoopStainOnMyShirt <joyojhyoty@gmail.com>
2025-11-30 20:07:30 +0530

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/joyojhyoty-portfolio-site/" : "/",
+  base: mode === "development" ? "/" : "/joyojhyoty-portfolio-site/",
   server: {
     host: "::",
     port: 8080,
